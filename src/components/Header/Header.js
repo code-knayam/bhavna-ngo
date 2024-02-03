aefcc262bd55b3606e4a90114b4ca0cf87ed2dd5
mayank jain <mayankjain1394@gmail.com>
2024-02-03 11:57:34 +0530

    Firebase setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import "./Header.scss";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function Header() {
+	return (
+		<Navbar expand="lg" className="bg-body-tertiary">
+			<Container>
+				<Navbar.Brand href="#home">
+					<img
+						alt=""
+						src="/img/logo.svg"
+						width="30"
+						height="30"
+						className="d-inline-block align-top"
+					/>
+				</Navbar.Brand>
+				<Nav className="me-auto">
+					<Nav.Link href="#home">Home</Nav.Link>
+					<Nav.Link href="#features">Our Work</Nav.Link>
+					<Nav.Link href="#pricing">Media</Nav.Link>
+					<Nav.Link href="#pricing">About</Nav.Link>
+				</Nav>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default Header;
